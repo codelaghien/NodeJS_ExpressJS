ca82366fef7f9680e0ca7707438e12533ab1cac9
codelaghien <codelaghien.github.io>
2021-01-30 21:38:21 -0800

    497

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,7 @@
 const User = require('../models/user.model');
 const people = require('../assets/people.json');
+const fs = require('fs');
+const fileName = 'chao.txt';
 
 class UserController {
 	get(req, res) {
@@ -9,15 +11,30 @@ class UserController {
 		const filterPeople = people.filter((person) =>
 			person.first_name.includes(filter)
 		);
-		return res
-			.status(200)
-			.json({ data: filterPeople, length: filterPeople.length });
+
+		try {
+			const data = fs.readFileSync(fileName, 'utf8');
+			// console.log(data);
+			return res.status(200).json({ data });
+		} catch (e) {
+			console.log('Error:', e.stack);
+			return res.status(200).json({ error: 'Không thể đọc file' });
+		}
 	}
 
 	post(req, res) {
 		const filter = req.body.filter;
-		console.log('filter', filter);
-		return res.status(200).json({ result: `Chào bạn abc` });
+		// console.log('filter', filter);
+		// fs.writeFile
+		fs.appendFile(fileName, `Chào bạn ${filter} \n`, function (err) {
+			if (err) {
+				return res
+					.status(200)
+					.json({ error: `Không thể ghi vào file` });
+			} else {
+				return res.status(200).json({ result: `Chào bạn ${filter}` });
+			}
+		});
 	}
 }
 
