c4af98f6133477bb4769f81f91b1348deff75d23
codelaghien <codelaghien.github.io>
2021-02-02 20:54:38 -0800

    Video 500, MySQL login trong file .env

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,23 +1,48 @@
 const User = require('../models/user.model');
 const people = require('../assets/people.json');
-
+const db = require('../database');
 class UserController {
 	get(req, res) {
-		// return res.status(200).json('Chào bạn, đã nhận được yêu cầu của bạn !');
-		const filter = req.query.filter;
-		// console.log('filter', filter);
-		const filterPeople = people.filter((person) =>
-			person.first_name.includes(filter)
-		);
-		return res
-			.status(200)
-			.json({ data: filterPeople, length: filterPeople.length });
+		db.connectDB()
+			.then((connection) => {
+				connection.query(
+					'SELECT * FROM users',
+					function (err, data, fields) {
+						db.closeDB(connection);
+						return res.status(200).json(data);
+					}
+				);
+			})
+			.catch((error) => {
+				console.log('Db not connected successfully', error);
+				return res
+					.status(200)
+					.json({ result: `Không thể kết nối Database` });
+			});
 	}
 
 	post(req, res) {
-		const filter = req.body.filter;
-		console.log('filter', filter);
-		return res.status(200).json({ result: `Chào bạn abc` });
+		const username = req.body.username;
+		const password = req.body.password;
+		// console.log('username', username);
+		// console.log('password', password);
+
+		db.connectDB()
+			.then((connection) => {
+				connection.query(
+					`INSERT INTO users(username, password, email) VALUES('${username}', '${password}', '')`,
+					function (err, data, fields) {
+						db.closeDB(connection);
+						return res.status(200).json({ result: `Thành công` });
+					}
+				);
+			})
+			.catch((error) => {
+				console.log('Db not connected successfully', error);
+				return res
+					.status(200)
+					.json({ result: `Không thể kết nối Database` });
+			});
 	}
 }
 
---
+++ database.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+require('dotenv').config();
+module.exports.connectDB = () => {
+	return new Promise((resolve, reject) => {
+		const con = mysql.createConnection({
+			host: process.env.DB_HOST || 'localhost',
+			user: process.env.DB_USER || 'myUserName',
+			password: process.env.DB_PASS || 'mypassword',
+			database: process.env.DB_NAME || 'mydb',
+		});
+		con.connect((err) => {
+			if (err) {
+				reject(err);
+			}
+			resolve(con);
+		});
+	});
+};
+module.exports.closeDB = (con) => {
+	con.destroy();
+};
