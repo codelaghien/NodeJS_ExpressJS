3615527b4d36c9114b89223aff98b2a4549f9cde
codelaghien <codelaghien.github.io>
2021-02-03 21:16:24 -0800

    BCrypt - Ma hoa password

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,8 @@
 const User = require('../models/user.model');
 const people = require('../assets/people.json');
 const db = require('../database');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
 class UserController {
 	get(req, res) {
 		db.connectDB()
@@ -24,16 +26,60 @@ class UserController {
 	post(req, res) {
 		const username = req.body.username;
 		const password = req.body.password;
-		// console.log('username', username);
-		// console.log('password', password);
+		let encryptedPassword = '';
+
+		bcrypt.hash(password, saltRounds, function (err, hash) {
+			encryptedPassword = hash;
+			console.log('hash', hash);
+
+			db.connectDB()
+				.then((connection) => {
+					connection.query(
+						`INSERT INTO users(username, password, email) VALUES('${username}', '${encryptedPassword}', '')`,
+						function (err, data, fields) {
+							db.closeDB(connection);
+							return res
+								.status(200)
+								.json({ result: `Thành công` });
+						}
+					);
+				})
+				.catch((error) => {
+					console.log('Db not connected successfully', error);
+					return res
+						.status(200)
+						.json({ result: `Không thể kết nối Database` });
+				});
+		});
+	}
+
+	login(req, res) {
+		const username = req.body.username;
+		const password = req.body.password;
 
 		db.connectDB()
 			.then((connection) => {
 				connection.query(
-					`INSERT INTO users(username, password, email) VALUES('${username}', '${password}', '')`,
+					`SELECT * FROM users WHERE username='${username}' LIMIT 1`,
 					function (err, data, fields) {
+						console.log('data', data[0].password);
 						db.closeDB(connection);
-						return res.status(200).json({ result: `Thành công` });
+
+						bcrypt.compare(
+							password,
+							data[0].password,
+							function (err, result) {
+								if (result) {
+									return res
+										.status(200)
+										.json('Login thành công');
+								} else {
+									return res
+										.status(200)
+										.json('Login thất bại');
+								}
+							}
+						);
 					}
 				);
 			})
--- routes/users.route.js
+++ routes/users.route.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user.controller');
 
 router.get('/', userController.get);
 router.post('/', userController.post);
+router.post('/login', userController.login);
 // router.put('/put', userController.put);
 
 module.exports = router;
